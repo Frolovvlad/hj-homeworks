66b9e76015fdc3fc7f6f1c83555f4428c71242d7
Vladislav Frolov <frolovvlad1995@gmail.com>
2018-04-29 16:24:58 +0300

    'version_1.3'

--- html-element-collection/drum-machine/js/index.js
+++ html-element-collection/drum-machine/js/index.js
@@ -4,7 +4,10 @@ const drumKit = document.getElementsByClassName('drum-kit')[0],
       buttons = drumKit.getElementsByTagName('li');
 
 function playMysic() {
-  this.getElementsByTagName('audio')[0].play()
+  const sound = this.getElementsByTagName('audio')[0];
+  sound.pause();
+  sound.currentTime = 0;
+  sound.play();
 }
 
 for(const btn of buttons) {
