7cd515bebede567cdd09383a01ef7af09c35f017
Vladislav Frolov <frolovvlad1995@gmail.com>
2018-05-21 14:36:01 +0300

    version_0.1

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,15 @@
 'use strict';
+
+list.addEventListener('click', function(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('add-to-cart')) {
+    let title = event.target.getAttribute('data-title'),
+        price = event.target.getAttribute('data-price');
+    addToCart({
+      title: title,
+      price: price
+    });
+  } else {
+    return;
+  }
+});
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const title = document.querySelectorAll('prop__name');
+
 function handleTableClick(event) {
-  
+  if (event.target.hasAttribute('data-prop-name')) {
+    let direction = event.target.getAttribute('data-dir') == 1 ? -1 : 1,
+        field = event.target.getAttribute('data-prop-name');
+
+    event.target.setAttribute('data-dir', direction);
+    event.target.setAttribute('data-sort-by', field);
+    sortTable(field, direction);
+  }
+  event.stopPropagation();
+}
+
+for (let th of title){
+  th.addEventListener('click', handleTableClick);
 }
