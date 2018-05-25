754077aced3e24a0ff05134d44dd47b7971b3ca7
Vladislav Frolov <frolovvlad1995@gmail.com>
2018-05-25 07:11:08 +0300

    version_1.0

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,36 @@
 'use strict';
+
+  const counter = document.getElementById('counter'),
+        increment = document.getElementById('increment'),
+        decrement = document.getElementById('decrement'),
+        reset = document.getElementById('reset');
+
+let step;
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  if (localStorage.step) {
+    step = localStorage.step;
+  } else {
+    step = 0;
+  }
+  counter.textContent = step;
+});
+
+
+increment.addEventListener('click', function() {
+	counter.textContent = ++step;
+  localStorage.step = step;
+});
+
+decrement.addEventListener('click', function() {
+	if (step > 0) {
+		counter.textContent = --step;
+    localStorage.step = step;
+	}
+});
+
+reset.addEventListener('click', function() {
+  localStorage.clear();
+  counter.textContent = 0;
+  step = 0;
+});
--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,61 @@
 'use strict';
+const signInHtm = document.querySelector('.sign-in-htm');
+const signUpHtm = document.querySelector('.sign-up-htm');
+const errorMessage = document.querySelector('.error-message');
+const buttonSignIn = signInHtm.querySelector('.button');
+buttonSignIn.addEventListener('click', function() {
+  const inputsSignIn = signInHtm.getElementsByTagName('input');
+  cteateMes(inputsSignIn);
+  fetch('https://neto-api.herokuapp.com/signin', {
+      body: JSON.stringify({
+        'email': emailSignIn.value,
+        'password': passSignIn.value
+      }),
+      credentials: 'same-origin', // 'include' | 'omit'
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function(res) {
+      let result = JSON.parse(res);
+      alert(`Пользователь ${result.name} успешно авторизован`);
+    })
+    .catch((err) => {
+      let error = JSON.parse(err);
+      errorMessage.innerHTML = error.message;
+    });
+});
+const buttonSignUp = signInHtm.querySelector('.button');
+const inputsSignUp = signUpHtm.getElementsByTagName('input');
+
+function cteateMes(filledForm) {
+  let form = {};
+  Array.from(filledForm)
+    .forEach(input => {
+      if (input.name) {
+        let item = input.name;
+        form[item] = input.value;
+      }
+    });
+}
+buttonSignUp.addEventListener('click', function() {
+  const inputsSignUp = signUpHtm.getElementsByTagName('input');
+  cteateMes(inputsSignUp);
+  fetch(' https://neto-api.herokuapp.com/signup', {
+      body: JSON.stringify(form),
+      credentials: 'same-origin', // 'include' | 'omit'
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function(res) {
+      let result = JSON.parse(res);
+      alert(`Пользователь ${result.name} успешно зарегистрирован`);
+    })
+    .catch((err) => {
+      let error = JSON.parse(err);
+      errorMessage.innerHTML = error.message;
+    });
+});
