8f50ee5a996a50957ded950426ee9bf93f7d1948
Vladislav Frolov <frolovvlad1995@gmail.com>
2018-05-28 08:56:26 +0300

    version_1.0

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', (event) => {
+  showBubbles(event.target);
+});
+
+document.addEventListener('click', (event) => {
+  connection.send(JSON.stringify({
+    x: event.clientX,
+    y: event.clientY
+  }));
+});
+
+window.addEventListener('beforeunload', () => {
+  connection.close()
+});
--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter'),
+      counter = document.querySelector('.counter'),
+      errors = document.querySelector('output.errors');
+
+connection.addEventListener('message', (event) => {
+  if(event.data) {
+    try {
+      const data = JSON.parse(event.data);
+      counter.textContent = data.connections;
+      errors.textContent = data.errors;
+    } catch (e) {
+      console.log(`Произошла ошибка "${e}"`)
+      return;
+    }
+  }
+});
+
+window.addEventListener('beforeunload', () => {
+  connection.close(1000, 'Сеанс завершен');
+});
